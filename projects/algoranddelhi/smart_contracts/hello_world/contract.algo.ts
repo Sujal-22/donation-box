d21c8e31175cd443d2457eb52a453e2c5836bfd5
Sujal kumar <157799236+Sujal-22@users.noreply.github.com>
2025-09-06 15:11:23 +0530

    first commit

--- projects/algoranddelhi/smart_contracts/hello_world/contract.algo.ts
+++ projects/algoranddelhi/smart_contracts/hello_world/contract.algo.ts
@@ -1,7 +1,18 @@
-import { Contract } from '@algorandfoundation/algorand-typescript'
+import { Contract, GlobalState, uint64, Txn } from "@algorandfoundation/algorand-typescript";
 
-export class HelloWorld extends Contract {
-  hello(name: string): string {
-    return `Hello, ${name}`
+export class DonationBox extends Contract {
+
+  // Global state to store total donations
+  totalDonations = GlobalState<uint64>({ key: "total", initialValue: 0 });
+
+  // Function: Donate Algo
+  donate(): string {
+    this.totalDonations.value = this.totalDonations.value + Txn.amount;
+    return "Thanks for donating! 🙏";
+  }
+
+  // Function: Check balance
+  checkTotal(): uint64 {
+    return this.totalDonations.value;
   }
 }
